ed3903ba94cdd534eb06e451a458cfe7eb59e123
Yazid <aadnanyazid@hotmail.fr>
2020-01-28 11:33:39 +0100

    outillage front : tp1

---
+++ outillages-frontend/tp1/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.set('engine view', 'ejs');
+
+
+app.get("/", function(req, res){
+    res.render('index.ejs',{listenom : ["Jean", "Paul", "Rene", "Fabienne", "Lucie"]} );
+});
+
+app.get("/profil/:nom", function(req, res){
+    res.render("profile.ejs", {nom : req.params.nom});
+});
+
+app.listen(3000, function(){
+    console.log("Adresse du serveur : http://localhost:3000");
+});
